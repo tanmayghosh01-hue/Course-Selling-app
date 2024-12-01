33ef1432bd2d3d204a1a843a12d7f2e976587580
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-12-01 12:27:34 +0530

    updated purchases

--- routes/course.js
+++ routes/course.js
@@ -4,29 +4,27 @@ const { purchaseModel } = require("../db");
 const courseRouter = Router();
 
 courseRouter.post("/purchase", userMiddleware, async (req, res) => {
-
   const userId = req.userId;
   const courseId = req.body.courseId;
 
   await purchaseModel.create({
     userId,
-    courseId
-  })
+    courseId,
+  });
 
   res.json({
-    message: "You have suxxcessfullie bought the course"
-  })
-})
+    message: "You have suxxcessfullie bought the course",
+  });
+});
 
 courseRouter.get("/preview", async (req, res) => {
-
-  const courses = await courseModel.find({})
+  const courses = await courseModel.find({});
 
   res.json({
-    courses
-  })
-})
+    courses,
+  });
+});
 
 module.exports = {
-  courseRouter: courseRouter
-}
+  courseRouter: courseRouter,
+};
